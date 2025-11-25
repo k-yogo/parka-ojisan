2397dfa522af727ce083e4419f9b707604aa04a4
yogo <8400185+k-yogo@users.noreply.github.com>
2025-11-26 00:37:58 +0900

    Fix Route

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
-Route::get('/create/', [PostController::class, 'create'])->name('posts.create');
+Route::get('/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/create/post', [PostController::class, 'store'])->name('posts.store');
