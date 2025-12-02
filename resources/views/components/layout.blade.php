6216789dcbc28067aa127b91e60b99d2d1418d86
yogo <8400185+k-yogo@users.noreply.github.com>
2025-12-02 17:38:20 +0900

    Add aria-label to header navigation links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,15 +15,17 @@
 
 <body class="flex flex-col min-h-dvh text-gray-950">
     <header class="p-4 flex justify-between items-center sticky top-0 bg-white z-10 border-b border-gray-200">
-        <h1><a href="{{ route('posts.index') }}" class="hover:opacity-70">パーカーおじさん</a></h1>
-        <a href="{{ route('posts.create') }}" class="hover:opacity-70"><svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="24"
-                height="24" fill="none" viewBox="0 0 24 24" id="new-post">
+        <h1><a href="{{ route('posts.index') }}" class="hover:opacity-70" aria-label="Home">パーカーおじさん</a></h1>
+        <a href="{{ route('posts.create') }}" class="hover:opacity-70" aria-label="New Post" title="New Post">
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                viewBox="0 0 24 24" id="new-post">
                 <path fill="currentColor" fill-rule="evenodd"
                     d="M1.5 7C1.5 3.96243 3.96243 1.5 7 1.5H17C20.0376 1.5 22.5 3.96243 22.5 7V17C22.5 20.0376 20.0376 22.5 17 22.5H7C3.96243 22.5 1.5 20.0376 1.5 17V7ZM7 2.5C4.51472 2.5 2.5 4.51472 2.5 7V17C2.5 19.4853 4.51472 21.5 7 21.5H17C19.4853 21.5 21.5 19.4853 21.5 17V7C21.5 4.51472 19.4853 2.5 17 2.5H7Z"
                     clip-rule="evenodd"></path>
                 <path fill="currentColor" fill-rule="evenodd" d="M11.5 18V6H12.5V18H11.5Z" clip-rule="evenodd"></path>
                 <path fill="currentColor" fill-rule="evenodd" d="M18 12.5H6V11.5H18V12.5Z" clip-rule="evenodd"></path>
-            </svg></a>
+            </svg>
+        </a>
     </header>
     <main class="w-full max-w-md mx-auto flex-1 flex flex-col">
         {{ $slot }}
